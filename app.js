06978b572e7fcbcf0d1a8d72e8bbc2a0d2ff7d84
Nandeep Shah <nandeep.shah@gmail.com>
2022-03-15 22:07:39 -0400

    fileupload

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const mongoose = require('mongoose');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 //monoose
 const url = config.mongoUrl;
@@ -61,6 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
